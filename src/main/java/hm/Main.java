867fea5ad4dc5d24afcc4b402539628e11e88562
Evgeniy Telepnev <telepnef@yandex.ru>
2023-01-11 21:00:45 +0300

    ДЗ

--- src/main/java/hm/Main.java
+++ src/main/java/hm/Main.java
@@ -6,6 +6,9 @@ public class Main {
         task_2();
         task_3();
         task_4();
+        task_5();
+        task_6();
+        task_7();
     }
 
     public static void task_1() {
@@ -16,6 +19,7 @@ public class Main {
         } else {
             System.out.println("Если возраст человека равен " + age + " вы не достигли совершеннолетия, нужно немного подождать");
         }
+        System.out.println("------------------------------------");
     }
 
     public static void task_2() {
@@ -26,6 +30,7 @@ public class Main {
         } else {
             System.out.println("На улице " + temper + " градусов, можно идти без шапки");
         }
+        System.out.println("------------------------------------");
     }
 
     public static void task_3() {
@@ -36,17 +41,16 @@ public class Main {
         } else {
             System.out.println("Если скорость " + speed + " , можно ездить спокойно");
         }
+        System.out.println("------------------------------------");
     }
 
     public static void task_4() {
         System.out.println("task - 4");
-        int age = 100;
+        int age = 30;
         boolean isNeedKindergarten = age>=2 && age<=6;
         boolean isGotoSchool = age>=7 && age<18;
         boolean isUniversity = age>18 && age<24;
         boolean isGotoWork = age>=24 && age <= 65;
-
-
 
         if(isNeedKindergarten) {
             System.out.println("Если возраст человека равен "+age+" , то ему нужно ходить в детский сад.");
@@ -60,29 +64,55 @@ public class Main {
         if(isGotoWork) {
             System.out.println("Если возраст человека равен "+age+" , то ему пора ходить на работу.");
         }
+        System.out.println("------------------------------------");
+    }
+    public static void task_5() {
+        System.out.println("task - 5");
+        int kidsAge = 11;
+
+        if(kidsAge<5) {
+            System.out.println("Если возраст ребенка равен " +kidsAge+ " , то ему нельзя кататься на аттракционе");
+        } if (kidsAge > 5 && kidsAge < 14) {
+            System.out.println("Если возраст ребенка равен " +kidsAge+ " , то ему можно кататься на аттракционе в сопровождении");
+        } if (kidsAge > 14) {
+            System.out.println("Если возраст ребенка равен " +kidsAge+ " , то он может кататься без сопровождения взрослого.");
+        }
+        System.out.println("------------------------------------");
     }
 
-    public static void tas() {
-        System.out.println("task - 4");
-        int age = 100;
-        boolean isNeedKindergarten = age>=2 && age<=6;
-        boolean isGotoSchool = age>=7 && age<18;
-        boolean isUniversity = age>18 && age<24;
-        boolean isGotoWork = age>=24 && age <= 65;
+    public static void task_6() {
+        System.out.println("task - 6");
+        int passengers = 100;
+        int oneVagon = 102;
+        boolean isSittingPlace = passengers <=60;
+        boolean isStandingPlace = passengers > 60 && passengers < oneVagon;
 
-
-
-        if(isNeedKindergarten) {
-            System.out.println("Если возраст человека равен "+age+" , то ему нужно ходить в детский сад.");
-        }
-        if(isGotoSchool) {
-            System.out.println("Если возраст человека равен "+age+" , то ему нужно ходить в ходить в школу.");
-        }
-        if(isUniversity) {
-            System.out.println("Если возраст человека равен "+age+" , то его место в университете.");
-        }
-        if(isGotoWork) {
-            System.out.println("Если возраст человека равен "+age+" , то ему пора ходить на работу.");
+        if(isSittingPlace) {
+            System.out.println("В вагоне есть сидячие места !");
+        } else if(isStandingPlace) {
+            System.out.println("В вагоне остались только стоячие места !");
+        } else {
+            System.out.println("В вагоне нет мест !!!");
         }
     }
+
+//    Здесь не поянл что нужно делать....
+    public static void task_7() {
+        System.out.println("task - 7");
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+        if (one < two) {
+            System.out.println("Один меньше двух");
+        } else if (two < three) {
+            System.out.println("Два меньше трех");
+        } else {
+            System.out.println("Три больше двух и одного");
+        }
+
+
+
+    }
+
 }
