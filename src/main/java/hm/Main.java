e3110d84aa1af9624cf4540fe4636b59961d97e1
Evgeniy Telepnev <telepnef@yandex.ru>
2023-01-12 08:06:25 +0300

    Сделана задача №2

--- src/main/java/hm/Main.java
+++ src/main/java/hm/Main.java
@@ -2,6 +2,32 @@ package hm;
 
 public class Main {
     public static void main(String[] args) {
-
+        task1_v2();
     }
+
+    public static void task1_v2() {
+        int clientOS = 0;
+        int iOS = 0;
+        int Android = 1;
+        int clientDeviceYear = 2010;
+
+
+        if (clientOS == iOS && (clientDeviceYear<= 2015  || clientDeviceYear >= 2015)){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if(clientDeviceYear > 2015) {
+                System.out.println("Установите приложение");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+        if (clientOS == Android && (clientDeviceYear<= 2015  || clientDeviceYear >= 2015)) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if(clientDeviceYear > 2015) {
+                System.out.println("Установите приложение");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
+    }
+
 }
