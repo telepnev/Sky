456e25ee0af07fe33b7548af414389610b40a689
Evgeniy Telepnev <telepnef@yandex.ru>
2023-02-15 12:20:55 +0300

    Готовая задача уровня Базовый

--- src/main/java/kursovik/kursovaya_1/MainEmployee.java
+++ src/main/java/kursovik/kursovaya_1/MainEmployee.java
@@ -4,9 +4,16 @@ public class MainEmployee {
 
     public static void main(String[] args) {
         EmployeeServes serves = new EmployeeServes();
-        serves.addEmployee("Evgeny", "Sergeevich", "Telepnev", 1, 1000);
-        serves.addEmployee("Petr", "Sergeevich", "Petrov", 2, 2000);
-        serves.addEmployee("Ivan", "Sergeevich", "Ivanov", 1, 3000);
+        serves.addEmployee("Евгений", "Сергеевич", "Телепнев", 1, 167000);
+        serves.addEmployee("Петр", "Сергеевич", "Сидоров", 2, 22000);
+        serves.addEmployee("Сергей", "Петрович", "Петров", 3, 32000);
+        serves.addEmployee("Анна", "Константиович", "Иванов", 5, 43000);
+        serves.addEmployee("Ольга", "Иванович", "Попов", 4, 33000);
+        serves.addEmployee("Константин", "Петрович", "Синицын", 4, 13000);
+        serves.addEmployee("Татьяна", "Евгеньевич", "Голицын", 1, 38000);
+        serves.addEmployee("Афанасий", "Сергеевич", "Теплицин", 2, 31000);
+        serves.addEmployee("Дмитрий", "Дмитрьевич", "Афанасьев", 3, 14000);
+        serves.addEmployee("Олег", "Олегович", "Разин", 6, 63000);
         serves.getFioEmployee();
         serves.printEmployeeList();
         serves.getAmountSalaryPerMonth();
